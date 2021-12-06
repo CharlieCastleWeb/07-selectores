e69afac01696d66e8b1063f1e3a2040a34356eb9
CharlieCastleWeb <castillomartinezcarlos@gmail.com>
2021-12-06 23:48:07 +0100

    Commit inicial Múltiples Selectores Anidados

---
+++ src/app/paises/pages/selector-page/selector-page.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PaisesService } from '../../services/paises.service';
+import { PaisSmall } from '../../interfaces/paises.interface';
+
+@Component({
+  selector: 'app-selector-page',
+  templateUrl: './selector-page.component.html',
+  styles: [
+  ]
+})
+export class SelectorPageComponent implements OnInit {
+
+  miFormulario: FormGroup = this.fb.group({
+    region: ['', Validators.required],
+    pais: ['', Validators.required]
+  })
+
+  // Llenar selectores
+  regiones: string[] = [];
+  paises: PaisSmall[] = [];
+
+  constructor(  private fb: FormBuilder,
+                private paisesService: PaisesService ) { }
+
+  ngOnInit(): void {
+    this.regiones = this.paisesService.regiones;
+
+    this.miFormulario.get('region')?.valueChanges
+      .subscribe( region => {
+        console.log(region);
+
+        this.paisesService.getPaisesPorRegion(region)
+          .subscribe(paises => {
+            console.log(paises);
+
+            this.paises = paises;
+          })
+      })
+  }
+
+  guardar() {
+    console.log(this.miFormulario.value);
+
+  }
+
+}
